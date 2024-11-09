fea8c80409bc02e72bdd6427bf7f70ac179d44b8
knowmad411dev <knowmad411dev@gmail.com>
2024-11-09 18:46:21 -0500

    trying dist in root

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Project_CDE/', // Update with your repository name
+  base: './', // Update with your repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
